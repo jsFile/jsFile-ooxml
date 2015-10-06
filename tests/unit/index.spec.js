31925558f788af427d20e2ce87b882281c136f21
webschik <webschik@gmail.com>
2015-10-06 21:04:30 +0200

    fix tests

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -24,23 +24,25 @@ describe('jsFile-ooxml', () => {
         const queue = [];
         for (let name in files) {
             if (files.hasOwnProperty(name)) {
-                const jf = new JsFile(files[name], {
-                    workerPath: '/base/dist/workers/'
-                });
-                const promise = jf.read()
-                    .then(done, done);
+                (function (file, name) {
+                    const jf = new JsFile(file, {
+                        workerPath: '/base/dist/workers/'
+                    });
+                    const promise = jf.read()
+                        .then(done, done);
 
-                queue.push(promise);
+                    queue.push(promise);
 
-                function done (result) {
-                    assert.instanceOf(result, JsFile.Document, name);
-                    const json = result.json();
-                    const html = result.html();
-                    const text = html.textContent || '';
-                    assert.jsonSchema(json, documentSchema, name);
-                    assert.notEqual(text.length, 0, `File ${name} shouldn't be empty`);
-                    assert.notEqual(result.name.length, 0, `Engine should parse a name of file ${name}`);
-                }
+                    function done (result) {
+                        assert.instanceOf(result, JsFile.Document, name);
+                        const json = result.json();
+                        const html = result.html();
+                        const text = html.textContent || '';
+                        assert.jsonSchema(json, documentSchema, name);
+                        assert.notEqual(text.length, 0, `File ${name} shouldn't be empty`);
+                        assert.notEqual(result.name.length, 0, `Engine should parse a name of file ${name}`);
+                    }
+                }(files[name], name));
             }
         }
 
